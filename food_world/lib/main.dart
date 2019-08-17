b69a3b8d4d8f6fa023647e0035177e8f613f83b0
ajay1706 <ajay97456@gmail.com>
2019-08-17 15:50:00 +0530

    adding Materia; app class

--- food_world/lib/main.dart
+++ food_world/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyHomePage());
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Daily Meals",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home:MyHomePage(),
+    );
+  }
+}
+
+
 
 class MyHomePage extends StatefulWidget {
   @override
