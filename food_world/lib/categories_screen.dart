231767aa9c712e35f37b5130c24cfa56e8a56000
ajay1706 <ajay97456@gmail.com>
2019-08-17 17:25:08 +0530

    adding all the styling to home screen & making categoryScreen as home screen

--- food_world/lib/categories_screen.dart
+++ food_world/lib/categories_screen.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
-
+import './dummy_data.dart';
+import './category_item.dart';
 
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily Meals"),
+        centerTitle: true,
+      ),
+      body: GridView(
+        padding:const EdgeInsets.all(25),
+        children:DUMMY_CATEGORIES.map(
+                (catData) => CategoryItem(
+                catData.title,
+                catData.color
+            )).toList()
 
 
-        
-
-
-      ],gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3/2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20
-    ),
+        ,gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+      ),
+      ),
     );
   }
 }
--- food_world/lib/main.dart
+++ food_world/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_world/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,34 +10,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Daily Meals",
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+title: TextStyle(
+  fontSize: 20,
+  fontFamily: 'RobotoCondensed',
+  fontWeight: FontWeight.bold
+)
+        )
       ),
-      home:MyHomePage(),
+      home:CategoriesScreen(),
     );
   }
 }
 
 
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Daily Meals"
-        ),
-      ),
-      body: Center(
-        child: Text(
-          "NAvigation"
-        ),
-      ),
-    );
-  }
-}
