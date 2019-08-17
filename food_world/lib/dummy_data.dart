fa22bbdafc63d585352803e7c21f2883a5a5b1e9
ajay1706 <ajay97456@gmail.com>
2019-08-17 22:08:26 +0530

    placing all the files in the proper folders

--- food_world/lib/dummy_data.dart
+++ food_world/lib/dummy_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './models/category.dart';
-import './meal.dart';
+import 'models/meal.dart';
 
 const DUMMY_CATEGORIES = const [
   Category(
--- food_world/lib/main.dart
+++ food_world/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_world/categories_screen.dart';
-import 'package:food_world/category_meals_screen.dart';
+import 'package:food_world/screens/categories_screen.dart';
+import 'package:food_world/screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
