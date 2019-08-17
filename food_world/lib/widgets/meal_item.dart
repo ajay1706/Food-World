009e6979ee7501837bd9df6ebd3148da7e34c18a
ajay1706 <ajay97456@gmail.com>
2019-08-18 00:47:26 +0530

    adding duration and affordable icons

--- food_world/lib/widgets/meal_item.dart
+++ food_world/lib/widgets/meal_item.dart
@@ -19,7 +19,42 @@ class MealItem extends StatelessWidget {
 });
 
 
-void selectMeal(){
+  String get complexityText {
+    switch (complexity){
+
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability){
+
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxorious';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+
+  void selectMeal(){
 
 }
 
@@ -64,17 +99,32 @@ void selectMeal(){
             Padding(
               padding: const EdgeInsets.all(15),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
 
                   Row(
                     children: <Widget>[
                       Icon(Icons.schedule,),
                       SizedBox(width:6 ,),
-                      Text('$duration'),
+                      Text('$duration min'),
 
                     ],
-                  )
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work,),
+                      SizedBox(width:6 ,),
+                      Text('$complexityText'),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.attach_money,),
+                          SizedBox(width:6 ,),
+                          Text('$duration'),
 
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
 
               ),
