729d4ca42a557fd6e3664c13120678dfe8319e2e
ajay1706 <ajay97456@gmail.com>
2019-08-17 23:00:01 +0530

    setting meal item and category meals screen

--- food_world/lib/screens/category_meals_screen.dart
+++ food_world/lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_world/widgets/meal_item.dart';
 import '../dummy_data.dart';
 class CategoryMealsScreen extends StatelessWidget {
 static const routeName = '/category-meals';
@@ -23,7 +24,10 @@ static const routeName = '/category-meals';
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle),),
       body: ListView.builder(itemBuilder: (ctx , index){
-        return Text(categoryMeals[index].title);
+        return MealItem(title: categoryMeals[index].title,imageUrl: categoryMeals[index].imageUrl,
+        duration: categoryMeals[index].duration,
+        affordability: categoryMeals[index].affordability,
+        complexity: categoryMeals[index].complexity,);
       },itemCount:categoryMeals.length ,)
     );}
 }
