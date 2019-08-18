29f3dd7d24b83abe1e9524eb738a0bafdb8810b0
ajay1706 <ajay97456@gmail.com>
2019-08-18 14:24:01 +0530

    adding up changes

--- food_world/lib/screens/tabs_screen.dart
+++ food_world/lib/screens/tabs_screen.dart
@@ -10,47 +10,31 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
+    return  Scaffold(
         appBar: AppBar(
           title: Text("Meals"),
-          bottom:TabBar(
-
-
-
-            tabs: <Widget>[
-
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-
-
-
-
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favourites',
-              ),
-
-
-
-            ],
-          ) ,
 
         ),
 
-        body: TabBarView(
-          children: <Widget>[
+        body: null,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.category),
+            title: Text(
+              'Categories'
+            )
+          ),
 
-            CategoriesScreen()
-            ,FavoritesScreen()
+          BottomNavigationBarItem(icon: Icon(Icons.category),
+              title: Text(
+                  'Categories'
+              )
+          ),
+        ],
 
-          ],
 
-        ),
+
       ),
     );
 }}
