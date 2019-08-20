8fdb1ea36dfec034f383a4e90fd079df161902f9
ajay1706 <ajay97456@gmail.com>
2019-08-20 16:54:18 +0530

    adding filter switches and their logic

--- food_world/lib/screens/filters_screen.dart
+++ food_world/lib/screens/filters_screen.dart
@@ -2,8 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:food_world/widgets/main_drawer.dart';
 
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+
+  // ignore: missing_return
+  Widget buildSwitchList(String title, String subTitle,bool currentValue,Function updateValue){
+   return SwitchListTile(
+        title: Text(
+       title
+    ),
+    value: currentValue,
+    subtitle: Text(subTitle),
+    onChanged: updateValue,);
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +37,50 @@ class FiltersScreen extends StatelessWidget {
         title: Text("Your Filters"),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text(
-          'Filters'
-        ),
-      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selections',
+              style:Theme.of(context).textTheme.title
+
+            ),
+          )
+          ,
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+
+             buildSwitchList("Gluten-free", "Only Gluten-free meals. ", _glutenFree,
+                 (newValue){
+               setState(() {
+                 _glutenFree=newValue;
+               });
+                 }),
+                buildSwitchList("Lactose-free", "Only Lactose-free meals. ", _lactoseFree,
+                        (newValue){
+                      setState(() {
+                        _lactoseFree=newValue;
+                      });
+                    }),
+                buildSwitchList("Vegetarian", "Only Vegetarian meals. ", _vegetarian,
+                        (newValue){
+                      setState(() {
+                        _vegetarian=newValue;
+                      });
+                    }),
+                buildSwitchList("Vegan", "Only Vegan meals. ", _vegan,
+                        (newValue){
+                      setState(() {
+                        _vegan=newValue;
+                      });
+                    })
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
