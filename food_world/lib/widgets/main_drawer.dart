345aeca3a59278f3c51089e9ff168ec3818b6f11
ajay1706 <ajay97456@gmail.com>
2019-08-20 13:45:56 +0530

    ading up filetrs screen and connecting the widgets of navigation bar

--- food_world/lib/widgets/main_drawer.dart
+++ food_world/lib/widgets/main_drawer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
 
-  Widget buildListTile(String title, IconData icon){
+  Widget buildListTile(String title, IconData icon,Function tapHandler){
     return   ListTile(
       leading:Icon(icon,size: 26,) ,
       title: Text(title,
@@ -12,7 +12,7 @@ class MainDrawer extends StatelessWidget {
             fontSize: 24,
             fontWeight: FontWeight.bold
         ),),
-      onTap: (){},
+      onTap: tapHandler,
     ) ;
 
 
@@ -41,8 +41,16 @@ child:Column(
     )
 ,
 SizedBox(height: 20,),
-    buildListTile("Meals", Icons.restaurant),
-    buildListTile("Filters", Icons.settings)
+    buildListTile("Meals", Icons.restaurant,
+      (){
+      Navigator.of(context).pushNamed('/')
+
+    }
+    ),
+    buildListTile("Filters", Icons.settings,
+            (){}
+    ),
+
 
 
   ],
